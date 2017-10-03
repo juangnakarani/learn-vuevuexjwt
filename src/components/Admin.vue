d91202b3cace32654c15addde4bf3db2651efee4
juangnakarani <juang.nakarani@gmail.com>
2017-10-03 10:59:07 +0700

    handling router on create life cycle admin

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -12,12 +12,20 @@
 </template>
 
 <script>
+import router from '@/router/index.js'
 export default {
   name: 'hello',
   data() {
     return {
       msg: 'Welcome to Admin Page'
     }
+  },
+  created: function () {
+    // `this` points to the vm instance
+    console.log('a is: ' + this.msg)
+    router.push({ path: '/403' })
+
+ 
   }
 }
 </script>
